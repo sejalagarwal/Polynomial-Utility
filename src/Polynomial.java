cfc164acd668bc99d740174c6417b20ac53d81dc
gkishan <gkishan@L-MAA-25005033.paypalcorp.com>
2016-08-05 08:51:45 +0530

    Polynomial Utility

---
+++ src/Polynomial.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Polynomial {
+	private HashMap<Integer, Integer> polynomial;
+
+	public Polynomial() {
+
+	}
+
+	public Polynomial(int input[][]) {
+
+	}
+
+	public Polynomial add(Polynomial p2) {
+		return null;
+	}
+
+	public Polynomial subtract(Polynomial p2) {
+		return null;
+	}
+
+	public Polynomial multiply(Polynomial p2) {
+		return null;
+	}
+
+	public String toString() {
+		String out = "";
+		return out;
+	}
+
+	public String toLatex() {
+		String out = "";
+		return out;
+	}
+
+	public String toHTML() {
+		String out = "";
+		return out;
+	}
+
+}
